5e8020dcea9fd1a876113509746f65e14df34f8f
Bryan Finster <bryan.finster@gmail.com>
2024-05-20 09:48:39 -0500

    chore: update dependencies

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,30 +1,3 @@
-module.exports = {
-	extends: ['@commitlint/config-conventional'],
-	rules: {
-		'type-enum': [
-			2,
-			'always',
-			[
-				'build',
-				'chore',
-				'ci',
-				'docs',
-				'feat',
-				'fix',
-				'perf',
-				'refactor',
-				'revert',
-				'style',
-				'test',
-				'feat!',
-				'fix!',
-				'refactor!'
-			]
-		],
-		'subject-case': [
-			2,
-			'always',
-			['sentence-case', 'start-case', 'pascal-case', 'upper-case', 'lower-case']
-		]
-	}
-}
+import commitlintConfig from '@defense-unicorns/website-commons/commitlint'
+
+export default commitlintConfig
